694ccfe26309d744919641d653408b4c6025b56e
이재영 <jaeyoung.yi@seculetter.com>
2023-09-05 08:16:34 +0900

    섹션 4. MVC 프레임워크 만들기 Model 추가 - v3

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -4,6 +4,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
+
 
 public class MyView {
 
@@ -15,9 +17,22 @@ public class MyView {
   }
 
 
-  public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  public void render(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
     request.getRequestDispatcher(viewPath)
            .forward(request, response);
+  }
+
+
+  public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    modelToRequestAttribute(model, request);
+    render(request, response);
+  }
+
+
+  private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+    model.forEach((k, v) -> request.setAttribute(k, v));
   }
 
 }
--- src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -32,7 +32,7 @@ public class FrontControllerServletV2 extends HttpServlet {
 
   @Override
   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    System.out.println("FrontControllerServletV1.service");
+    System.out.println("FrontControllerServletV3.service");
 
     String requestURI = request.getRequestURI();
     ControllerV2 controller = controllerMap.get(requestURI);
